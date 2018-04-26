9aeac6008677ca323b541737e1d97d284a80d5a9
cheezm91 <cheezm91@gmail.com>
2018-04-25 23:22:17 -0400

    Update schemas.sql

--- schemas.sql
+++ schemas.sql
@@ -1,80 +1,130 @@
 USE compose;
 
+DROP TABLE Users;
 CREATE TABLE Users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    userID INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(16) NOT NULL,
     acctType VARCHAR(20) DEFAULT 'Regular'
 );
 
+DROP TABLE RegisteredAccts;
 CREATE TABLE RegisteredAccts(
     userID INT PRIMARY KEY,
     address VARCHAR(255) NOT NULL,
     f_name VARCHAR(50) NOT NULL,
     l_name VARCHAR(50) NOT NULL,
-    vip_status BOOLEAN,
-    rating DECIMAL(2,1),
-    FOREIGN KEY(userID) REFERENCES Users(id)
+    status INT DEFAULT 1, /*0 = blacklisted, 1 = regular, 2 = VIP*/
+    rateNum INT DEFAULT 0,
+    rateSum DECIMAL DEFAULT 0.0,
+    rating DECIMAL(2,1) DEFAULT 0.0,
+    FOREIGN KEY(userID) REFERENCES Users(userID)
 );
 
+DROP TABLE PaymentInfo;
 CREATE TABLE PaymentInfo(
     userID INT PRIMARY KEY,
-    creditNum INT,
-    ccv INT,
-    experation DATE,
-    FOREIGN KEY(userID) REFERENCES Users(id)
+    name VARCHAR(50) NOT NULL,
+    creditNum INT NOT NULL,
+    ccv INT NOT NULL,
+    expiration DATE NOT NULL,
+    FOREIGN KEY(userID) REFERENCES Users(userID)
 );
 
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- Anything below this comment is questionable/ looks complicated to work with and should probably be changed
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+DROP TABLE Managers;
 CREATE TABLE Managers(
     userID INT PRIMARY KEY,
     restaurantID INT,
-    FOREIGN KEY(userID) REFERENCES Users(id)
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
 );
 
+DROP TABLE Cooks;
+CREATE TABLE Cooks(
+    userID INT,
+    restaurantID INT ,
+    strikes INT DEFAULT 0,
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
+);
+
+DROP TABLE DeliveryPerson;
+CREATE TABLE DeliveryPerson(
+    userID INT,
+    restaurantID INT ,
+    rateNum INT DEFAULT 0,
+    rateSum DECIMAL DEFAULT 0.0,
+    rating DECIMAL(2,1) DEFAULT 0.0,
+    FOREIGN KEY(userID) REFERENCES Users(id),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
+);
+
+DROP TABLE Restaurants;
 CREATE TABLE Restaurants(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    address VARCHAR(255),
+    restaurantID INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
-    rating DECIMAL(2,1),
-    telephoneNum INT
+    address VARCHAR(255),
+    phoneNum VARCHAR(15),
+    rateNum INT DEFAULT 0,
+    rateSum DECIMAL DEFAULT 0.0,
+    rating DECIMAL(2,1) DEFAULT 0.0
 );
 
+DROP TABLE Menu;
 CREATE TABLE Menu(
     restaurantID INT PRIMARY KEY,
-    foodID INT,
+    foodName VARCHAR(255) PRIMARY KEY,
     price DECIMAL(4,2),
-    FOREIGN KEY(restaurantID) REFERENCES Restaurants(id),
-    FOREIGN KEY(foodID) REFERENCES Food(id),
+    description VARCHAR(500),
+    rateNum INT,
+    rateSum DECIMAL(2,1),
+    rating DECIMAL(2,1),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
 );
 
-CREATE TABLE Food(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255)
+DROP TABLE Orders;
+CREATE TABLE Orders(
+    orderID INT PRIMARY KEY AUTO_INCREMENT,
+    userID INT, /*userID is 0 for guests, so no reference to userID table*/
+    restaurantID INT,
+    orderDate DATE,
+    status INT DEFAULT 0, /*0 = pending order, 1 = filled order/pending delivery, 2 = delivered*/
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
 );
 
-CREATE TABLE Cook(
-    userID INT REFERENCES Users(id),
-    restID INT REFERENCES Restaurants(id),
-    stikes INT DEFAULT 0
+DROP TABLE FoodInOrder;
+CREATE TABLE FoodInOrder(
+    orderID INT,
+    foodName VARCHAR(255),
+    FOREIGN KEY(orderID) REFERENCES Orders(orderID),
+    FOREIGN KEY(foodName) REFERENCES Menu(foodName)
 );
 
+DROP TABLE Favorites;
 CREATE TABLE Favorites(
-    id INT PRIMARY KEY,
-    userID INT REFERENCES Users(id),
-    restID INT REFERENCES Restaurants(id)
+    favoriteID INT PRIMARY KEY AUTO_INCREMENT,
+    userID INT,
+    restaurantID INT,
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID)
 );
 
+DROP TABLE FoodInFavorites;
 CREATE TABLE FoodInFavorites(
-    id INT,
-    foodID INT,
-    FOREIGN KEY(foodID) REFERENCES Food(id)
+    favoriteID INT,
+    foodName VARCHAR(255),
+    FOREIGN KEY(favoriteID) REFERENCES Favorites(favoriteID),
+    FOREIGN KEY(foodName) REFERENCES Menu(foodName)
 );
 
-CREATE TABLE CurrentOrders(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    foodID INT REFERENCES Food(id)
+DROP TABLE Complaints;
+CREATE TABLE Complaints(
+    userID INT,
+    restaurantID INT,
+    foodName VARCHAR(255),
+    complaint VARCHAR(500),
+    rating DECIMAL(2,1),
+    FOREIGN KEY(userID) REFERENCES Users(userID),
+    FOREIGN KEY(restaurantID) REFERENCES Restaurants(restaurantID),
+    FOREIGN KEY(foodName) REFERENCES Menu(foodName)
 );
