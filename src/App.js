91860f736a65b47939de0603906ed23823d6d0e4
Andy Wang <Andy@Andys-MacBook-Pro.local>
2018-04-27 14:40:49 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Component/Routes/Routes';
 
 class App extends Component {
+  // Hi There
    render() {
         return (
           <BrowserRouter>
