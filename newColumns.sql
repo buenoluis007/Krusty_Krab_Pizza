8f3972fa5c072d1a0448bcee5c09a494bc013485
buenoluis007 <33500965+buenoluis007@users.noreply.github.com>
2018-05-02 14:44:11 -0400

    Add files via upload

---
+++ newColumns.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Restaurants
+ADD COLUMN longitude VARCHAR(15) AFTER address;
+
+ALTER TABLE Restaurants
+ADD COLUMN latitude VARCHAR(15) AFTER longitude;
+
+ALTER TABLE Restaurants
+ADD COLUMN googleID VARCHAR(100) AFTER latitude;
