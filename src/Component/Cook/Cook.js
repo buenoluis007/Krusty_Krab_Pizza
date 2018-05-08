cd633bd6c40202662c77b89f67fabbcb99d33425
Evan Vasquez <32246279+EvanVasquez@users.noreply.github.com>
2018-05-07 22:50:04 -0400

    Minor changes

---
+++ src/Component/Cook/Cook.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Cook.css'
+
+class Cook extends Component{
+  render(){
+    return(
+      <div className='Cook'>
+          <h1> Cook Section of the site </h1>
+      </div>
+    );
+  }
+}
+
+export default Cook;
