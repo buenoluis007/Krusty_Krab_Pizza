defcc59c11ccfa27ed2033e2956e85e3aba78f84
Andy Wang <andywang219@yahoo.com>
2018-05-09 01:15:28 -0400

    Update Manager.js

--- src/Component/Manager/Manager.js
+++ src/Component/Manager/Manager.js
@@ -27,7 +27,6 @@ class Manager extends PureComponent {
                 < Cooks />
                 < DeliveryPerson />
                 < Pending />
-            < Direction />
             </div>
         )
     }
